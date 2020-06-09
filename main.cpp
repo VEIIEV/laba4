a991ce2c7b71b9a5579c33460b4e64e98c06c0a8
VEIIEV <62066130+VEIIEV@users.noreply.github.com>
2020-06-09 13:58:48 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "doublesidequeue.h"
+using namespace std;
+
+int main()
+{
+	DoubleSideQueue<int> queue_1;
+	queue_1.print();
+	cout << "\n";
+	queue_1.pushBack(1);
+	queue_1.pushBack(2);
+	queue_1.pushFront(3);
+	queue_1.print();
+	cout << "\nFront: " << queue_1.topFront() << "\tBack: " << queue_1.topBack() << "\n\n";
+	queue_1.popBack();
+	queue_1.print();
+	cout << "\n";
+	queue_1.popFront();
+	queue_1.print();
+	cout << "\n";
+
+	DoubleSideQueue<int> queue_2(queue_1);
+	queue_2.print();
+	cout << "\n";
+	queue_2.clear();
+	queue_2.print();
+	cout << "\n";
+	queue_2 = queue_1;
+	queue_2.print();
+	return 0;
+}
